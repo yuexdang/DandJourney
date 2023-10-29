3d7822a648cfd72f20e0a72e2ccc440d5bc66a14
yuexdang <56034408+yuexdang@users.noreply.github.com>
2023-10-29 12:43:30 +0800

    HotFix 231029

--- App/apis/DiscordSpider/utils/Globals.py
+++ App/apis/DiscordSpider/utils/Globals.py
@@ -1,16 +1,17 @@
 Imagine = {
     "session_id" : "2fb980f65e5c9a77c96ca01f2c242cf6",
-    "version" : "1118961510123847772",
+    "version" : "1166847114203123795",
     "id" : "938956540159881230",
 }
 
 Morph = {
-    "upsample" : "45bc04dd4da37141a5f73dfbfaf5bdcf",
+    "upsample" : "cca46572757a91150ad7b45f1c816c58",
     "variation" : "1f3dbdf09efdf93d81a3a6420882c92c",
     "reroll" : "712c631b97b9133f3eb51dce5a5db9d1",
     "remaster" : "3e9a692ad1bb3570861bf14d2bd8b7fe",
     "upsample_light" : "7ae8ee8ef6ffa44fb4d9702a88fd93ed",
     "upsample_beta" : "7ae8ee8ef6ffa44fb4d9702a88fd93ed",
+    "Inpaint" : "d5520ec1979a8b0ab58e3f5a13277fcd",
 }
 
 Fast = {
@@ -27,12 +28,12 @@ Relax = {
 
 Blend = {
     "session_id" : "6105f01666b0ca19aabc13c9862c638c",
-    "version" : "1118961510123847773",
+    "version" : "1166847114203123796",
     "id" : "1062880104792997970",
 }
 
 Describe = {
     "session_id" : "5be666160a2dc210a99c460d38c30a61",
-    "version" : "1118961510123847774",
+    "version" : "1166847114203123797",
     "id" : "1092492867185950852",
 }
